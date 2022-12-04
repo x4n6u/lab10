6aeef673599386e12c182ad11c4025baead259bc
Joshua Law <85807286+x4n6u@users.noreply.github.com>
2022-12-03 18:43:36 -0700

    Completed
    Lab 10 Completed can be changed later

--- portfolio-app/src/Header.js
+++ portfolio-app/src/Header.js
@@ -1,25 +1,28 @@
+import picture from './picture.jpg';
+
+
 function Header() {
     return (
         <header className="text-white bg-dark">
         <div className="container py-5">
             <div className="row">
                 <div className="col-xs-3 col-lg-2 text-center">
-                    <img src="https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"
+                    <img src={picture}
                         alt="Person - Avatar Placeholder@seekpng.com" className="border border-5 rounded-circle img-fluid" style={{ width: "150px", height: "150px" }}/>
                 </div>
                 <div className="col text-center text-lg-start">
-                    <h1>Joe Blow</h1>
+                    <h1>Joshua Law</h1>
 
                     <p className="lead">Software development student at SAIT (Southern Alberta Institute of Technology).</p>
 
                     <ul className="list-inline">
-                        <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
+                        <li className="list-inline-item my-2"><a href="mailto: Joshuasmlaw@gmail.com" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-envelope pe-1"></i>
                                 <span className="text-center">E-mail</span></a></li>
                         <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-linkedin pe-1"></i>
                                 LinkedIn</a></li>
-                        <li className="list-inline-item my-2"><a href="#" className="fs-5 link-light text-decoration-none"><i
+                        <li className="list-inline-item my-2"><a href="https://github.com/x4n6u" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-github pe-1"></i>
                                 GitHub</a></li>
                     </ul>
